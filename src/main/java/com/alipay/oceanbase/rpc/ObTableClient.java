9bad99149758781ba3150750d9323f57d59d803a
JackShi148 <jackshi896@gmail.com>
2025-04-10 21:01:06 +0800

    rebase secondary_part master

--- src/main/java/com/alipay/oceanbase/rpc/ObTableClient.java
+++ src/main/java/com/alipay/oceanbase/rpc/ObTableClient.java
@@ -364,7 +364,7 @@ public class ObTableClient extends AbstractObTableClient implements Lifecycle {
         // build tableRoster and ServerRoster
         TableEntryKey rootServerKey = new TableEntryKey(clusterName, tenantName,
                 OCEANBASE_DATABASE, ALL_DUMMY_TABLE);
-        tableRoute.initRoster(rootServerKey, initialized);
+        tableRoute.initRoster(rootServerKey, initialized, runningMode);
         // create background refresh-checker task
         tableRoute.launchRouteRefresher();
     }
@@ -2705,7 +2705,7 @@ public class ObTableClient extends AbstractObTableClient implements Lifecycle {
         NORMAL, HBASE;
     }
 
-    private ObTableClientType getClientType(RunningMode runningMode) {
+    public ObTableClientType getClientType(RunningMode runningMode) {
         if (ObGlobal.isDistributedExecSupport()) {
             return runningMode == RunningMode.HBASE ? ObTableClientType.JAVA_HBASE_CLIENT : ObTableClientType.JAVA_TABLE_CLIENT;
         } else {
